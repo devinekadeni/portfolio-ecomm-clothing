9331a25bdc72e55be638b58ab7602faa0c9437e3
Devin <devinekadeni@gmail.com>
2019-08-01 00:47:47 +0700

    added sign in form layout

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+
+import './sign-in.styles.scss'
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+
+    this.setState({ email: '', password: '' })
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target
+
+    this.setState({ [name]: value })
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            type="email"
+            name="email"
+            value={this.state.email}
+            label="email"
+            onChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type="password"
+            name="password"
+            value={this.state.password}
+            label="password"
+            onChange={this.handleChange}
+            required
+          />
+
+          <CustomButton type="submit">Sign in</CustomButton>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SignIn
