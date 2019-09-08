172f5369d93333546efa74be48f5d2e66a523eb3
Devin <devinekadeni@gmail.com>
2019-09-08 23:14:19 +0700

    setup redux-thunk and replace fetch shop data with redux-thunk

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,63 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
 import { Route } from 'react-router-dom'
+import { createStructuredSelector } from 'reselect'
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component'
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
-import { updateCollections } from '../../redux/shop/shop.actions'
+import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions'
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
 
 const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview)
 const CollectionPageWithSpinner = WithSpinner(CollectionPage)
 
 class ShopPage extends React.Component {
-  state = {
-    isLoading: true,
-  }
-
-  unsubscribeFromSnapshot = null
-
   componentDidMount() {
-    const { updateCollection } = this.props
-
-    const collectionRef = firestore.collection('collections')
-
-    collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-      updateCollection(collectionsMap)
-      this.setState({ isLoading: false })
-    })
+    const { fetchCollectionsStartAsync } = this.props
+    fetchCollectionsStartAsync()
   }
 
   render() {
-    const { match } = this.props
-    const { isLoading } = this.state
+    const { match, isCollectionFetching } = this.props
     return (
       <div className="shop-page">
         <Route
           exact
           path={`${match.path}`}
           render={props => (
-            <CollectionOverviewWithSpinner isLoading={isLoading} {...props} />
+            <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} />
           )}
         />
         <Route
           path={`${match.path}/:categoryId`}
-          render={props => <CollectionPageWithSpinner isLoading={isLoading} {...props} />}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props} />
+          )}
         />
       </div>
     )
   }
 }
 
+const mapStateToProps = createStructuredSelector({
+  isCollectionFetching: selectIsCollectionFetching,
+})
+
 const mapDispatchToProps = dispatch => ({
-  updateCollection: collectionsMap => dispatch(updateCollections(collectionsMap)),
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
 })
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(ShopPage)
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shop.types'
 
-export const updateCollections = collections => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collections,
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
 })
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap,
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+})
+
+export const fetchCollectionsStartAsync = () => dispatch => {
+  dispatch(fetchCollectionsStart())
+  const collectionRef = firestore.collection('collections')
+
+  collectionRef
+    .get()
+    .then(async snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+      dispatch(fetchCollectionsSuccess(collectionsMap))
+      this.setState({ isLoading: false })
+    })
+    .catch(err => dispatch(fetchCollectionsFailure(err)))
+}
