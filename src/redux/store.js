517db1c344e927d67a4334e5c9957c7c4c2b28d3
Devin <devinekadeni@gmail.com>
2019-09-17 02:18:34 +0700

    implement redux-saga for fetching shop data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
-import thunk from 'redux-thunk'
+// import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 
+import { fetchCollectionsStart } from './shop/shop.sagas'
 import rootReducer from './root-reducer'
 
-const middlewares = [thunk]
+const sagaMiddleware = createSagaMiddleware()
+
+// const middlewares = [thunk]
+const middlewares = [sagaMiddleware]
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger)
@@ -14,6 +19,8 @@ if (process.env.NODE_ENV === 'development') {
 const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(rootReducer, composeEnchancers(applyMiddleware(...middlewares)))
+
+sagaMiddleware.run(fetchCollectionsStart)
 
 const persistor = persistStore(store)
 
