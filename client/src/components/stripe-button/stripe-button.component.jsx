a50c282e214e445c65e8401a0038a47d214b358b
Devin <devinekadeni@gmail.com>
2019-10-04 16:17:54 +0700

    moved all of front end code into /client folder

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
+import axios from 'axios'
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100
   const publishableKey = 'pk_test_B8Xveze3CT7ouy2jvKco8DDC006XDo8KiL'
   const onToken = token => {
-    console.log(token)
-    alert('payment Successful')
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      }
+    }).then(resp => {
+      console.log('es',resp)
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error))
+      alert('there was an issue with your payment. Please sure you use the proper card')
+    })
   }
 
   return (
