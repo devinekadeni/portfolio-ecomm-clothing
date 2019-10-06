38ae372f919ed0d412e40a857c9ef8d3820e76f1
Devin <devinekadeni@gmail.com>
2019-10-06 20:30:52 +0700

    changed shop collection from redux into context

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { connect } from 'react-redux'
 
 import { selectCollection } from '../../redux/shop/shop.selectors'
 import CollectionItem from '../../components/collection-item/collection-item.component'
 
+import CollectionsContext from '../../contexts/collections/collections.context'
+
 import './collection.styles.scss'
 
-const CollectionPage = ({ collection }) => {
-  console.log('col', collection)
+const CollectionPage = ({ match }) => {
+  // Context Component Way - START
+  // return (
+  //   <CollectionsContext.Consumer>
+  //     {collections => {
+  //       const collection = collections[match.params.categoryId]
+  //       const { title, items } = collection
+
+  //       return (
+  //         <div className="collection-page">
+  //           <h2 className="title">{title}</h2>
+  //           <div className="items">
+  //             {items.map(item => (
+  //               <CollectionItem key={item.id} item={item} />
+  //             ))}
+  //           </div>
+  //         </div>
+  //       )
+  //     }}
+  //   </CollectionsContext.Consumer>
+  // )
+  // Context Component Way - FINISH
+
+  // Context Hooks Way - START
+  const collections = useContext(CollectionsContext)
+  const collection = collections[match.params.categoryId]
   const { title, items } = collection
+
   return (
     <div className="collection-page">
       <h2 className="title">{title}</h2>
@@ -19,10 +46,11 @@ const CollectionPage = ({ collection }) => {
       </div>
     </div>
   )
+  // Context Hooks Way - FINISH
 }
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.categoryId)(state),
 })
 
-export default connect(mapStateToProps)(CollectionPage)
+export default CollectionPage
