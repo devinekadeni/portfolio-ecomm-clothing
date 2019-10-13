428c10579b636f536252e10532a48a154df7bd90
Devin <devinekadeni@gmail.com>
2019-10-13 16:58:42 +0700

    implemented pwa compliant; added service worker; enforce https in herokiu

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const path = require('path')
 const compression = require('compression')
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
@@ -18,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // url string doesn't contain
 app.use(cors())
 
 if (process.env.NODE_ENV === 'production') {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }))
   /**
    *  will be able to server every files on below (path.join(__dirname, 'client/build')
    */
@@ -27,6 +29,10 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
   })
 }
+
+app.get('/service-worker.js', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'))
+})
 
 app.post('/payment', (req, res) => {
   const body = {
